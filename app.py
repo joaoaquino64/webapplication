2f6ca6320e1874165ebe76e4a425fdb9e08e7717
joaoaquino64 <joaovitorthaquino@gmail.com>
2022-06-03 21:16:57 +0000

    adicionei comentarios

--- app.py
+++ app.py
@@ -7,6 +7,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World'
+    return render_template('index.html') 
 
-app.run()
+app.run(debug=True)
+
+
+
+
+
+# CLIENTE -- SERVIDOR
+# Navegador -- AWS (Flask)
+
+# Client -> REQUEST (MENSAGEM HTTP) -> Server 
+# Server -> RESPONSE (MENSAGEM HTTP) -> CLIENTE
+
+# HTTP (HyperText Transfer Protocol)
+# HTML (HyperText Markup Language)
+
+# Mensagem HTTP:
+# Header
+# Body
+# METHOD (GET, POST), Métodos suportados pelos navegadores.
+# GET -> DADOS PELA URL
+# POST -> OCULTO OS DADOS (NAO MOSTRA URL)
+
+# METHOD (API = GET, POST, PUT, DELETE, PATCH, OPTIONS)   
+
+# REST    
+# POST    (C)REATE
+# GET     (R)EAD
+# PUT     (U)PDATE
+# PATH    (U)PDATE PARCIAL
+# DELETE  (D)ELETE 
