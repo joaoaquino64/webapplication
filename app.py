27a8168d725cce97768d142f8126b9cfc83dd916
joaoaquino64 <joaovitorthaquino@gmail.com>
2022-06-07 00:06:32 +0000

    Criei a variavel dogs e criei a função 'home'

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 # Ao abrir o GitPod, execute:
 # pip install -r requirements.txt
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+dogs = [
+    {'id': 1, 'nome': 'Thor', 'raça': 'Yorkshire', 'status': 'adoção'},
+    {'id': 2, 'nome': 'Lola', 'raça': 'Shitzu', 'status': 'adoção'},
+    {'id': 3, 'nome': 'Joaquim', 'raça': 'Buldog frances', 'status': 'adoção'}
+
+]
+
+
 @app.route('/')
-def hello():
-    return render_template('index.html') 
+def home():
+    return render_template('home.html', dogs=dogs) 
 
 app.run(debug=True)
 
