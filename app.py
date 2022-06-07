6b0333d56797d63b7847f0b0de800a739ed68d1d
joaoaquino64 <joaovitorthaquino@gmail.com>
2022-06-07 23:47:19 +0000

    criei o arquivo dogs.csv

--- app.py
+++ app.py
@@ -2,14 +2,20 @@
 # pip install -r requirements.txt
 
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
+
 
 dogs = [
     {'id': 1, 'nome': 'Thor', 'raça': 'Yorkshire', 'status': 'adoção'},
     {'id': 2, 'nome': 'Lola', 'raça': 'Shitzu', 'status': 'adoção'},
     {'id': 3, 'nome': 'Joaquim', 'raça': 'Buldog francês', 'status': 'adoção'}
 ]
+
+with open('dogs.csv', 'wt') as file_out:
+    escritor = csv.writer(file_out)
+    escritor.writeows(dogs)
 
 @app.route('/')
 def home():
