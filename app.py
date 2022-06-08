3df54c76356cd3c96ea5b5c14fced4fba9a1e4f2
joaoaquino64 <joaovitorthaquino@gmail.com>
2022-06-08 23:01:05 +0000

    Criei a aba de adicionar e coloquei um gif

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
 # Ao abrir o GitPod, execute:
 # pip install -r requirements.txt
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from uuid import uuid4
 import csv
 
 app = Flask(__name__)
 
 
-dogs = [
-    {'id': 1, 'nome': 'Thor', 'raça': 'Yorkshire', 'status': 'pronto para adoção'},
-    {'id': 2, 'nome': 'Lola', 'raça': 'Shitzu', 'status': 'necessita vacinação'},
-    {'id': 3, 'nome': 'Joaquim', 'raça': 'Buldog francês', 'status': 'necessita cuidados especiais'}
-]
+dogs = [{'id': uuid4(), 'Nome': 'Joaquim', 'Raça': 'Lulu da Pomerânia', 'Cor': 'Castanho', 'Idade': '6 meses', 'Status do cão': 'Pronto para adoção'}]
 
-with open('dogs.csv', 'wt') as file_out:
-    escritor = csv.writer(file_out)
+with open('dogs.csv', 'w') as file_out:
+    escritor = csv.DictWriter(file_out, ['id', 'Nome', 'Raça', 'Cor', 'Idade', 'Status do cão'])
+    escritor.writeheader()
     escritor.writerows(dogs)
 
 @app.route('/')
 def home():
     return render_template('home.html', dogs=dogs) 
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
 
 app.run(debug=True)
