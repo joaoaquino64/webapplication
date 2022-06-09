2f2c9f84a32600717e9d31b172b93bf5f936b4e0
joaoaquino64 <joaovitorthaquino@gmail.com>
2022-06-09 00:58:01 +0000

    Adicionei a funcão de criar formulario

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ import csv
 app = Flask(__name__)
 
 
-dogs = [{'id': uuid4(), 'Nome': 'Joaquim', 'Raça': 'Lulu da Pomerânia', 'Cor': 'Castanho', 'Idade': '6 meses', 'Status do cão': 'Pronto para adoção'}]
+dogs = [{'id': uuid4(), 'Nome': 'Joaquim', 'Raça': 'Lulu da Pomerânia', 'Cor': 'Castanho', 'Idade': '6 meses', 'Status': 'Pronto para adoção'}]
 
 with open('dogs.csv', 'w') as file_out:
-    escritor = csv.DictWriter(file_out, ['id', 'Nome', 'Raça', 'Cor', 'Idade', 'Status do cão'])
+    escritor = csv.DictWriter(file_out, ['id', 'Nome', 'Raça', 'Cor', 'Idade', 'Status'])
     escritor.writeheader()
     escritor.writerows(dogs)
 
@@ -22,6 +22,16 @@ def home():
 @app.route('/add')
 def add():
     return render_template('add.html')
+
+@app.route('/save', methods=['POST'])
+def save():
+    nome = request.form['Nome']
+    raça = request.form['Raça']
+    cor = request.form['Cor']
+    idade = request.form['Idade']
+    status = request.form['Status']
+    dogs.append({'id': uuid4(), 'Nome': nome, 'Raça': raça, 'Cor': cor, 'Idade': idade, 'Status': status})
+    return render_template('home.html', dogs=dogs)
 
 
 app.run(debug=True)
